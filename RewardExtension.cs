9202a30a151b7acd1c368f205f21d3b0a32fd57e
RoyIL <a4834833@gmail.com>
2016-06-17 18:01:05 +0300

    Update

--- RewardExtension.cs
+++ RewardExtension.cs
@@ -12,11 +12,13 @@ using Rocket.API;
 
 namespace fr34kyn01535.RewardExtension
 {
+
     public class RewardExtension : RocketPlugin<RewardExtensionConfiguration>
     {
+        public static RewardExtension Instance = null;
         protected override void Load()
         {
-
+            Instance = this;
             if (IsDependencyLoaded("Uconomy"))
             {
                 Logger.Log("Optional dependency Uconomy is present.");
@@ -26,7 +28,7 @@ namespace fr34kyn01535.RewardExtension
                 Logger.Log("Optional dependency Uconomy is not present.");
             }
             {
-
+                Votifier.Votifier.Instance.OnPlayerVoted += Instance_OnPlayerVoted;
 
 
 
@@ -39,7 +41,7 @@ namespace fr34kyn01535.RewardExtension
                 return new TranslationList()
                 {
                     { "votifierxp_player_has_voted","Thanks for voting on {0}, you have received {1}" } ,
-                        { "votifierxp_Creator"," XP / Money Option By http://goo.gl/cz32QX " } ,
+                        { "votifierxp_creator"," XP / Money Option By http://goo.gl/cz32QX " } ,
                        { "votifierxpuconomy_player_has_voted","Thanks for voting on {0}, you have received {1} and now have {2} in your balance." }
 
 
@@ -53,38 +55,34 @@ namespace fr34kyn01535.RewardExtension
         {
 
             {
-                Votifier.Votifier.Instance.OnPlayerVoted += Instance_OnPlayerVoted;
+
 
                 string XPReceived = Configuration.Instance.XP + "Sup";
 
                 player.Experience += Configuration.Instance.XP.Value;
 
-                UnturnedChat.Say(player, Translate("votifierxp_player_has_voted", definition.Name, XPReceived));
+                UnturnedChat.Say(player, Instance.Translations.Instance.Translate("votifierxp_player_has_voted", definition.Name, XPReceived));
                 Logger.Log(player.DisplayName + " has received " + XPReceived + " because he voted on " + definition.Name);
-                UnturnedChat.Say(player, Translate("votifierxp_Creator"));
+                UnturnedChat.Say(player, Instance.Translations.Instance.Translate("votifierxp_creator"));
                 Logger.Log("XP / Money Option By http://goo.gl/cz32QX");
             }
             {
-                Votifier.Votifier.Instance.OnPlayerVoted += Instance_OnPlayerVoted;
+
                 RewardExtension.ExecuteDependencyCode("Uconomy", (IRocketPlugin plugin) =>
-
-
-
-
-
                 {
+
                     string moneyReceived = Configuration.Instance.Money + Uconomy.Uconomy.Instance.Configuration.Instance.MoneyName;
                     string newTotalMoney = Uconomy.Uconomy.Instance.Database.IncreaseBalance(player.Id, Configuration.Instance.Money) + Uconomy.Uconomy.Instance.Configuration.Instance.MoneyName;
 
-
-                    UnturnedChat.Say(player, Translate("votifierxpuconomy_player_has_voted", definition.Name, moneyReceived, newTotalMoney));
+                    UnturnedChat.Say(player, Instance.Translations.Instance.Translate("votifierxpuconomy_player_has_voted", definition.Name, moneyReceived, newTotalMoney));
                     Logger.Log(player.DisplayName + " has received " + moneyReceived + " because he voted on " + definition.Name);
-
                 });
+
             }
         }
     }
 }
+
     
 
              
